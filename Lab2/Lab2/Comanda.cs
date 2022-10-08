bda135fb35d14183ec6c14295c8b3e535723fb53
DanielMonoranu <monoranu_dani@yahoo.ro>
2022-10-08 14:33:03 +0300

    added homework

---
+++ Lab2/Lab2/Comanda.cs
@@ -0,0 +1,21 @@
+﻿using CSharp.Choices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    [AsChoice] //pt a folosi CSharp Choices 
+    public static partial class Produs
+    {
+       // public record Contact(string Nume, string Prenume, string NrTelefon, string Adresa);
+
+        public interface IQuantity{} //interfata marker, artificiala, pt constructii de genu.
+        public record Kg(float kg):IQuantity;
+        public record Unit(int unit):IQuantity;
+
+
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharp.Choices;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Diagnostics.CodeAnalysis;
+using static Lab2.Comanda;
+
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        IQuantity qty;
+        qty = new Kg(12.5f);
+        //qty.match(when kg: q =q.kg
+        //                          )
+        var result= qty.Match(whenKg: q => q.kg,
+                   whenUnit:q=> q.unit);
+
+        Console.WriteLine(qty);
+        Console.WriteLine(result);
+
+    }
+}
