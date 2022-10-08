f49428f4aeab30a0a26c1e47ffa1ff06dd74ec01
DanielMonoranu <monoranu_dani@yahoo.ro>
2022-10-08 14:15:01 +0300

    added homework

---
+++ LAB2/DatcLab2/DatcLab2/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace DatcLab2
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public string Year { get; set; }
+
+    }
+}
